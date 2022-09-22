62408d70493b22dbf3ad9fc4815c363d7fe7915e
Philip Njuguna <pnjuguna.main@gmail.com>
2022-09-22 16:23:08 +0100

    Added variable to ask for name

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world philip njuguna");
+        Scanner scanner = new Scanner(System.in);
+        String name = null;
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+
+        System.out.println("Hello world "+name);
     }
 }
